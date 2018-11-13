c08211ca410d1c61595eef9a64aabf8f507624cc
Rawoof <rawoofsharief.muthujasherif@syncfusion.com>
2018-11-13 12:54:14 +0530

    Sample commit

---
+++ FadingEdge/Fading/Fading/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Syncfusion.ListView.XForms;
+using Xamarin.Forms.Internals;
+
+namespace Grouping
+{
+    [Preserve(AllMembers = true)]
+    public class ListViewGroupingViewModel
+    {
+        #region Fields
+
+        private ObservableCollection<ListViewContactsInfo> contactsInfo;
+
+        #endregion
+
+        #region Constructor
+
+        public ListViewGroupingViewModel()
+        {
+            GenerateSource(100);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<ListViewContactsInfo> ContactsInfo
+        {
+            get { return contactsInfo; }
+            set { this.contactsInfo = value; }
+        }
+
+        #endregion
+
+        #region ItemSource
+
+        public void GenerateSource(int count)
+        {
+            ListViewContactsInfoRepository contactRepository = new ListViewContactsInfoRepository();
+            contactsInfo = contactRepository.GetContactDetails(count);
+        }
+
+        #endregion
+    }
+}
